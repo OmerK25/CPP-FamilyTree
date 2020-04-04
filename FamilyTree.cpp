cb65aa030e9340fafdbb953a2ff8cbd3dcbfcf5b
Omer <omer2katz@gmail.com>
2020-04-04 13:51:09 +0300

    base

---
+++ FamilyTree.cpp
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <iostream>
+#include "FamilyTree.hpp"
+
+namespace family
+{
+class Tree
+{
+    private:
+    string name;
+
+public:
+Tree(string n){
+name =n;
+}
+
+
+}
+} // namespace family
---
+++ FamilyTree.hpp
@@ -0,0 +1,5 @@
+using namespace std; 
+
+namespace family{
+void Tree(string r);
+}
